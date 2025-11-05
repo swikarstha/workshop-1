956611766cbc5d65e0a740f7161242d6a668f538
email <swikarstha5844@gmail.com>
2025-11-05 12:19:33 +0545

    task 2

---
+++ task 2/Program.cs
@@ -0,0 +1,26 @@
+﻿class Circle
+{
+    const double PI = 3.14;
+
+    public static void Main(string[] args)
+    {
+        PI = 3;
+         Console.WriteLine(PI);
+        
+
+        Console.WriteLine(area(2));
+        Console.WriteLine(perimeter(3));
+    }
+
+    static double area(double radius)
+    {
+        return PI * radius * radius;
+    }
+
+    static double perimeter(double radius)
+    {
+        return 2 * PI * radius;
+    }
+
+
+}
